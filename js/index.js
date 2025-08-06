961442550923e841758862826e4b3f8033ddc53a
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-08-06 17:08:39 +0900

    슬라이드 크기 및 화살표 변경

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
+let index = 0;
+let timer;
+
 $(function () {
   const $wrap = $(".slide-wrap");
   const $slides = $(".slide");
   const slideWidth = $(".slide").width();
-  let index = 0;
-  let timer;
 
-  // 첫 슬라이드 복제 → 맨 뒤에 추가 (무한 루프)
   $wrap.append($slides.first().clone());
 
   // 다음 버튼
@@ -32,17 +32,8 @@ $(function () {
     $wrap.animate({ marginLeft: -index * slideWidth }, 500);
   }
 
-  // 자동 슬라이드
   function startAuto() {
     timer = setInterval(nextSlide, 3000);
   }
-  function stopAuto() {
-    clearInterval(timer);
-  }
-
-  // 자동 시작
   startAuto();
-
-  // 마우스 올리면 멈춤, 내리면 다시 시작
-  $("#gallery").hover(stopAuto, startAuto);
 });
