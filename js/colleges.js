d7ecb5f89f113353ee2d9afec946fab66a4c492e
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-08-11 12:43:54 +0900

    학과 상세페이지 생성

--- js/colleges.js
+++ js/colleges.js
@@ -1,15 +1,36 @@
 $(function () {
   $(".college-department").click(collegeTab);
+  $("[id^='department-']").click(departmentModal);
+  $("#modal-close").click(() => $("#department-modal").removeClass("show"));
+  $("#department-modal").click(function (e) {
+    if (this === e.target) {
+      $(this).removeClass("show");
+    }
+  });
 });
 
 function collegeTab() {
   const targetId = $(this).data("college");
 
-  // 다른 리스트는 active 클래스 제거해서 숨김 처리
   $(".department-list")
     .not("#" + targetId)
     .removeClass("active");
 
-  // 클릭한 대상은 active 토글 (보이기/숨기기)
   $("#" + targetId).toggleClass("active");
+}
+
+function departmentModal() {
+  const collegeId = $(this).closest(".department-list").attr("id");
+  const departmentId = $(this).attr("id").replace("department-", "");
+
+  const url = `content/${collegeId}/${departmentId}.html`;
+
+  $("#modal-body").html("<p>로딩 중...</p>");
+  $("#department-modal").addClass("show");
+
+  $("#modal-body").load(url, function (response, status) {
+    if (status === "error") {
+      $("#modal-body").html("<p>정보를 불러오는 데 실패했습니다.</p>");
+    }
+  });
 }
